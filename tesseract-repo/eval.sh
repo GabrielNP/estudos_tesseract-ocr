8fbc9242d47097d4908046e9ab44610570cdd0b3
Gabriel Pessighello <gabriel.pessighello@indico.net.br>
2020-11-26 22:09:55 -0300

    Organinzing repo

--- tesseract-repo/eval.sh
+++
@@ -1,3 +0,0 @@
-OMP_THREAD_LIMIT=8 lstmeval --model output-por/por_checkpoint \
-    --traineddata tesseract/tessdata/por.traineddata \
-    --eval_listfile train-por/por.training_files.txt
--- tesstrain-repo/batches/rename.sh
+++
@@ -1,3 +0,0 @@
-for file in ../data/por-ground-truth/*.tif; do
-    python3 rename.py $file;
-    done
